dab54575fca3aaeb47c8d3509d7e93fa0b8ad3ec
ryanvasq325 <ryanvasques77@gmail.com>
2026-05-15 15:59:41 -0400

    Commit

---
+++ app/helpers/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+session_start();
+
+define('HOST', $_SERVER['HTTP_HOST']);
+
+define('ROOT', dirname(__FILE__, 3));
+#DIRETÓRIO DAS VIEWS
+define('DIR_VIEWS', ROOT . '/app/view');
+#EXTENSÃO PADRÃO DAS VIEWS
+define('EXT_VIEWS', '.html');
+#Chave secreta para geração de tokens
+define('SECRET_KEY', '58ae142d-afae-4443-994a-43f2bef0e366');
